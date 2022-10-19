49c5fb4239ad09349f90d21e3c770a2d80f5c3dc
Bin He <emptyhb@gmail.com>
2022-10-19 11:19:33 -0500

    wrote shinyapp to visualize multiple hit correction

--- 03-asset/seq-div/Estimate_DNA_divergence/app.R
+++ 03-asset/seq-div/Estimate_DNA_divergence/app.R
@@ -8,6 +8,7 @@
 #
 
 library(shiny)
+library(dplyr)
 
 # Define functions for simulating evolution
 mutate <- function(seq){
@@ -28,7 +29,20 @@ evolve <- function(seq, N){
     seqs <- c(seqs, list(seq)) # record the genotype at this generation
     Nobs <- c(Nobs, sum(seqs[[1]] != seq)) # this records the number of *observed* mutations
   }
-  return(list(seqs, 0:N, Nobs))
+  return(list(s = seqs, realized = 0:N, observed = Nobs))
+}
+
+printSeq <- function(seq){
+  return(paste0(c("A", "C", "G", "T")[seq+1], collapse = ""))
+}
+
+printAlign <- function(seqs){
+  original <- c("A", "C", "G", "T")[seqs[[1]]+1]
+  final <- c("A", "C", "G", "T")[seqs[[length(seqs)]]+1]
+  align <- ifelse(final == original, ".", final)
+  s0 <- paste(original, collapse = "")
+  s1 <- paste(align, collapse = "")
+  return(paste(s0, s1, sep = "\n"))
 }
 
 # Define UI for application that draws a histogram
@@ -40,38 +54,74 @@ ui <- fluidPage(
     # Sidebar with a slider input for number of bins 
     sidebarLayout(
         sidebarPanel(
-            numericInput("seqL",
+            sliderInput("seqL",
                          "Sequence length (nt):",
                          min = 1,
-                         max = 100,
-                         value = 30),
-            sliderInput("mutation",
-                        "Number of mutations:",
-                        min = 0,
-                        max = 300,
-                        value = 1)
+                         max = 200,
+                         value = 50),
+            numericInput("mutN",
+                         "Number of mutations:",
+                         min = 0,
+                         value = 30,
+                         step = 5),
+            actionButton("refresh", "Recalculate")
         ),
 
         # Show a plot of the generated distribution
         mainPanel(
-           plotOutput("distPlot")
+          p("The following alignment shows the final sequence (bottom) in relation to the original (top)."),
+          verbatimTextOutput("align"),
+          em("Number of actual mutations:"),
+          textOutput("real"),
+          em("Number of observed mutations:"),
+          textOutput("obs"),
+          em("Corrected # of mutations (Jukes-Cantor):"),
+          textOutput("corr"),
+          plotOutput("relation")
         )
     )
 )
 
 # Define server logic required to draw a histogram
 server <- function(input, output) {
-
-    output$distPlot <- renderPlot({
-        # generate bins based on input$bins from ui.R
-        x    <- faithful[, 2]
-        bins <- seq(min(x), max(x), length.out = input$bins + 1)
-
-        # draw the histogram with the specified number of bins
-        hist(x, breaks = bins, col = 'darkgray', border = 'white',
-             xlab = 'Waiting time to next eruption (in mins)',
-             main = 'Histogram of waiting times')
-    })
+  # define initial sequence
+  s = sample(0:3, size = 200, replace = TRUE)
+  
+  # reactive expression to re-execute the mutation experiment when seq length or # mutation parameters change
+  dataInput <- reactive({
+    input$refresh # simply by accessing the input$refresh button makes this chunk dependent on it
+    seq <- s[1:input$seqL]
+    evolve(seq, input$mutN)
+  })
+  
+  output$align = renderPrint({
+    seqs <- dataInput()$s
+    original <- c("A", "C", "G", "T")[seqs[[1]]+1]
+    final <- c("A", "C", "G", "T")[seqs[[length(seqs)]]+1]
+    align <- ifelse(final == original, ".", final)
+    s0 <- paste(original, collapse = "")
+    s1 <- paste(align, collapse = "")
+    cat(paste(s0, s1, sep = "\n"))
+  })
+  
+  output$real <- renderText({ input$mutN })
+  
+  output$obs <- renderText({
+    tmp <- dataInput()
+    last(tmp$observed)
+  })
+  
+  output$corr <- renderText({
+    tmp <- dataInput()
+    obs <- last(tmp$observed)
+    L <- input$seqL
+    c <- -3/4*log(1-4/3*obs/L)
+    K <- c*L
+  })
+  output$relation <- renderPlot({
+    tmp <- dataInput()
+    plot(tmp$realized, tmp$observed, type = "b", xlab = "Actual # of mutations", ylab = "Observed # of mutations")
+  })
 }
 
 # Run the application 
